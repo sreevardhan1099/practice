73c51d28d9fe94d7869577d808d8b53e4e0d0916
vardhan <sreevardhan1099@gmail.com>
2022-12-23 15:13:29 -0500

    third stmt

--- src/main/java/com/example/springfirstapp/Test1.java
+++ src/main/java/com/example/springfirstapp/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("check che3ck");
         System.out.println("second");
+        System.out.println("third");
     }
 }
